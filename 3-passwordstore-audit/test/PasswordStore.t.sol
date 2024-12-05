ee4939130f4325421faf7562480dbca4a0f26d5f
gaozilong <gaozilong@pinefield-inc.com>
2024-12-05 10:05:56 +0800

    [ADD] unit test

---
+++ 3-passwordstore-audit/test/PasswordStore.t.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.18;
+
+import {Test, console} from "forge-std/Test.sol";
+import {PasswordStore} from "../src/PasswordStore.sol";
+import {DeployPasswordStore} from "../script/DeployPasswordStore.s.sol";
+
+contract TestPasswordStore is Test {
+    PasswordStore public passwordStore;
+    DeployPasswordStore public deployer;
+    address public owner;
+
+    function setUp() public {
+        deployer = new DeployPasswordStore();
+        passwordStore = deployer.run();
+        owner = msg.sender;
+    }
+
+    function test_anyone_can_set_password(address randomAddress) public {
+        vm.prank(randomAddress);
+        string memory expectedPassword = "myNewPassword";
+        passwordStore.setPassword(expectedPassword);
+
+        vm.prank(owner);
+        string memory actualPassword = passwordStore.getPassword();
+        assertEq(actualPassword, expectedPassword);
+    }
+}
